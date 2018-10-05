3e1eda35021cb8bfc554408a7fe4e142f01797ca
Daniel MC <daniel.meneses27@gmail.com>
2018-10-05 06:55:20 -0500

    first iteration in react example. create app, add bootstrap dependence, create 3 components to show and handle navbar, counters and counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
 import './App.css';
+import NavBar from './components/navbar'
+import Counters from './components/counters';
 
 class App extends Component {
+
+  state = {
+    counters: [
+      { id: 1, qty: 0 },
+      { id: 2, qty: 0 },
+      { id: 3, qty: 0 },
+      { id: 4, qty: 0 }
+    ]
+  };
+
+  handleDeleteQty = id => {
+    const counters = this.state.counters.filter(counter => counter.id !== id);
+    this.setState({
+      counters
+    });
+  };
+
+  handleIncrementCounter = counter => {
+    let counters = this.state.counters;
+    const index = counters.indexOf(counter);
+    counters[index].qty++;
+    this.setState({
+      counters
+    });
+  };
+
+  handleResetQty = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.qty = 0;
+      return counter;
+    });
+    this.setState({
+      counters
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar
+          counters={this.state.counters.filter(c => c.qty > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onDeleteQty={this.handleDeleteQty}
+            counters={this.state.counters}
+            onResetQty={this.handleResetQty}
+            onIncrementCounter={this.handleIncrementCounter} />
+        </main>
+      </React.Fragment>
     );
+
   }
 }
 
