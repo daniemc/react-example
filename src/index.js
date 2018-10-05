caebc819198de6e7f6b8fefa4693afccc73cd5d8
daniemc <daniel.meneses27@gmail.com>
2018-10-05 15:19:41 -0500

    shopping cart icon

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import { faStroopwafel, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faStroopwafel)
+library.add(faStroopwafel, faShoppingCart)
 
 ReactDOM.render( < App / > , document.getElementById('root'));
