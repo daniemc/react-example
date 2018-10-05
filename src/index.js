7c4cdc9250dcad6e019be047e62faa6266a22bba
daniemc <daniel.meneses27@gmail.com>
2018-10-05 14:43:51 -0500

    add fontawsome icons support

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
 
 ReactDOM.render( < App / > , document.getElementById('root'));
